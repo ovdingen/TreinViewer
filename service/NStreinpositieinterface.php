148053271bf2cb321fe0279696809a5ae695981e
Hillebrand van de Groep <me@hillebrand.io>
2018-01-05 14:07:36 +0100

    Added popup contents to geojson feature

--- service/NStreinpositieinterface.php
+++ service/NStreinpositieinterface.php
@@ -7,12 +7,13 @@ $subscriber = new ZMQSocket($context, ZMQ::SOCKET_SUB);
 $subscriber->connect("tcp://pubsub.besteffort.ndovloket.nl:7664");
 $subscriber->setSockOpt(ZMQ::SOCKOPT_SUBSCRIBE, "/RIG/NStreinpositiesInterface5");
 
-function maakGeoJSONFeatureVanTreinMaterieelDeel(SimpleXMLElement $materieeldeel) {
+function maakGeoJSONFeatureVanTreinMaterieelDeel(SimpleXMLElement $materieeldeel, $treinnummer) {
     $properties = array();
     $properties['materieelnummer'] = $materieeldeel->MaterieelDeelNummer[0];
     $properties['materieelvolgnummer'] = $materieeldeel->MaterieelVolgNummer;
     $properties['snelheid'] = $materieeldeel->Snelheid[0];
     $properties['richting'] = $materieeldeel->Richting[0];
+    $properties['popupContent'] = "<b>Trein " . $treinnummer . "<br>Materieelnummer " . $properties['materieelnummer'] . "<br>" . "Snelheid: " . $properties['snelheid'] . "km/h<br>" . "Richting: " . $properties['richting'] . " graden";
 
     $point = new \GeoJson\Geometry\Point([(float) $materieeldeel->Longitude, (float) $materieeldeel->Latitude]);
 
@@ -28,13 +29,13 @@ while (true) {
     $features = array();
     foreach($data as $key => $value) {
         if($key == "TreinLocation") {
-            $trainNumber = $value->TreinNummer;
+            $trainNumber = (string) $value->TreinNummer;
             if(is_array($value->TreinMaterieelDelen)) {
                 foreach($value->TreinMaterieelDelen as $materieeldeel) {
-                    array_push($features, maakGeoJSONFeatureVanTreinMaterieelDeel($materieeldeel));
+                    array_push($features, maakGeoJSONFeatureVanTreinMaterieelDeel($materieeldeel, $trainNumber));
                 }
             } else {
-                array_push($features, maakGeoJSONFeatureVanTreinMaterieelDeel($value->TreinMaterieelDelen));
+                array_push($features, maakGeoJSONFeatureVanTreinMaterieelDeel($value->TreinMaterieelDelen, $trainNumber));
             }
         }
     }
