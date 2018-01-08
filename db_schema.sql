2372bfe6aa905acb6282d2270a387e32e0752c79
Hillebrand van de Groep <me@hillebrand.io>
2018-01-08 22:01:56 +0100

    updated db schema

--- db_schema.sql
+++ db_schema.sql
@@ -1,1 +1,1 @@
-CREATE TABLE arnu(receive INT NOT NULL, servicecode SMALLINT NOT NULL, transportmodecode VARCHAR(12), servicetype VARCHAR(40), contents TEXT);
+CREATE TABLE arnu(receive INT NOT NULL, servicecode INT NOT NULL, transportmodecode VARCHAR(12), servicetype VARCHAR(40), contents TEXT);
